1920aa78ae42d42d88881f977560b23eb0ae4a89
Tarxos <ftarxos@gmail.com>
2021-04-22 01:11:24 +0300

    add swagger for testing api

--- src/Microservices/MetricsManager/MetricsManager/Startup.cs
+++ src/Microservices/MetricsManager/MetricsManager/Startup.cs
@@ -10,6 +10,8 @@ namespace MetricsManager
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+            services.AddSwaggerGen(c => c.SwaggerDoc("v1", new() {Title = "Metrics manager api", Version = "v1"}));
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -17,13 +19,14 @@ namespace MetricsManager
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger().UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json","Metrics manager api v1"));
             }
 
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
+                endpoints.MapControllers();
             });
         }
     }

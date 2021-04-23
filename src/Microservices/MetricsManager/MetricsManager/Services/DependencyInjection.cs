affc667e8612074fe8b9cbff7bfc97c3752817c7
Tarxos <ftarxos@gmail.com>
2021-04-24 01:02:53 +0300

    remove weather api

--- src/Microservices/MetricsManager/MetricsManager/Services/DependencyInjection.cs
+++ src/Microservices/MetricsManager/MetricsManager/Services/DependencyInjection.cs
@@ -5,7 +5,7 @@ namespace MetricsManager.Services
     public static class DependencyInjection
     {
         public static IServiceCollection AddServices(this IServiceCollection services) => services
-            .AddSingleton<WeatherStore>()
+            
         ;
     }
 }

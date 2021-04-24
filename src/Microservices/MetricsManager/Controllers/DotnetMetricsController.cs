a92f8f3cc90ad8a31265a26689a23edb3fb07bee
Tarxos <ftarxos@gmail.com>
2021-04-25 01:48:09 +0300

    Metrics manager dotnet metrics controller and it's unit tests

---
+++ src/Microservices/MetricsManager/Controllers/DotnetMetricsController.cs
@@ -0,0 +1,44 @@
+using System;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MetricsManager.Controllers
+{
+    [Route("api/metrics/dotnet")]
+    public class DotnetMetricsController : ApiController
+    {
+        [HttpGet("agent/{agentId}/from/{fromTime}/to/{toTime}")]
+        public IActionResult GetMetricsFromAgent(
+            [FromRoute] int agentId, 
+            [FromRoute] TimeSpan fromTime, 
+            [FromRoute] TimeSpan toTime)
+        {
+            return Ok();
+        }
+
+        [HttpGet("agent/{agentId}/from/{fromTime}/to/{toTime}/percentiles/{percentile}")]
+        public IActionResult GetMetricsByPercentileFromAgent(
+            [FromRoute] int agentId, 
+            [FromRoute] TimeSpan fromTime, 
+            [FromRoute] TimeSpan toTime,
+            [FromRoute] Percentile percentile)
+        {
+            return Ok();
+        }
+
+        [HttpGet("cluster/from/{fromTime}/to/{toTime}")]
+        public IActionResult GetMetricsFromAllCluster([FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime)
+        {
+            return Ok();
+        }
+
+        [HttpGet("cluster/from/{fromTime}/to/{toTime}/percentiles/{percentile}")]
+        public IActionResult GetMetricsByPercentileFromAllCluster(
+            [FromRoute] TimeSpan fromTime,
+            [FromRoute] TimeSpan toTime,
+            [FromRoute] Percentile percentile)
+        {
+            return Ok();
+        }
+    }
+}

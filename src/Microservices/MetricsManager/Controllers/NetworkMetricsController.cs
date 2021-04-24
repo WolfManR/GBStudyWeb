b844d895321e617285b18532b5d7468ab0af8dd4
Tarxos <ftarxos@gmail.com>
2021-04-25 02:08:16 +0300

    Metrics manager network controller and it tests

---
+++ src/Microservices/MetricsManager/Controllers/NetworkMetricsController.cs
@@ -0,0 +1,26 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MetricsManager.Controllers
+{
+    [Route("api/metrics/network")]
+    public class NetworkMetricsController : ApiController
+    {
+        [HttpGet("agent/{agentId}/from/{fromTime}/to/{toTime}")]
+        public IActionResult GetMetricsFromAgent(
+            [FromRoute] int agentId, 
+            [FromRoute] TimeSpan fromTime, 
+            [FromRoute] TimeSpan toTime)
+        {
+            return Ok();
+        }
+
+        [HttpGet("cluster/from/{fromTime}/to/{toTime}")]
+        public IActionResult GetMetricsFromAllCluster(
+            [FromRoute] TimeSpan fromTime, 
+            [FromRoute] TimeSpan toTime)
+        {
+            return Ok();
+        }
+    }
+}

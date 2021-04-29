263a34423bd2cf88ac29d59cc835c37c9079ff17
Tarxos <ftarxos@gmail.com>
2021-04-30 01:16:08 +0300

    change TimeSpan to DateTimeOffset, and add cpu metrics controller behavior, and update it's structs

--- src/Microservices/MetricsAgent/Controllers/CpuMetricsController.cs
+++ src/Microservices/MetricsAgent/Controllers/CpuMetricsController.cs
@@ -1,15 +1,41 @@
+using System.Linq;
+using Common;
 using MetricsAgent.Controllers.Requests;
+using MetricsAgent.Controllers.Responses;
+using MetricsAgent.DataBase.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace MetricsAgent.Controllers
 {
     [Route("api/metrics/cpu")]
     public class CpuMetricsController : ApiController
     {
+        private readonly ICpuMetricsRepository _repository;
+        private readonly ILogger<CpuMetricsController> _logger;
+
+        public CpuMetricsController(ICpuMetricsRepository repository, ILogger<CpuMetricsController> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+        
+        
         [HttpGet("from/{fromTime}/to/{toTime}")]
         public IActionResult GetMetrics([FromRoute] CpuMetricsRequest request)
         {
-            return Ok();
+            _logger.LogInformation(
+                LogEvents.RequestReceived,
+                "Get cpu metrics by time period request received: {From}, {To}"
+                ,request.FromTime.ToString("h:mm:ss tt zz"),
+                request.ToTime.ToString("h:mm:ss tt zz"));
+
+            var result = _repository.GetByTimePeriod(request.FromTime, request.ToTime);
+            if (result is null)
+            {
+                return NotFound();
+            }
+            return Ok(new CpuMetricsByTimePeriodResponse(){Metrics = result.Select(m => m.Value).ToList()});
         }
     }
 }
--- src/Microservices/MetricsAgent/DataBase/Models/CpuMetrics.cs
+++ src/Microservices/MetricsAgent/DataBase/Models/CpuMetrics.cs
@@ -5,7 +5,7 @@ namespace MetricsAgent.DataBase.Models
     public class CpuMetrics : IEntity<int>
     {
         public int Id { get; init; }
-        public TimeSpan Time { get; set; }
+        public DateTimeOffset Time { get; set; }
         public int Value { get; set; }
     }
 }

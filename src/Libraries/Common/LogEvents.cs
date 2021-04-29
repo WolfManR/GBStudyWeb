86bc524c2e9fb0c784eca2112ddda8dcdf7adcf1
Tarxos <ftarxos@gmail.com>
2021-04-29 21:50:12 +0300

    LogEvents

---
+++ src/Libraries/Common/LogEvents.cs
@@ -0,0 +1,16 @@
+﻿namespace Common
+{
+    public static class LogEvents
+    {
+        public const int EntityCreationSuccess = 100;
+        public const int EntityCreationFailure = 101;
+        
+        public const int EntityObtainingSuccess = 102;
+        public const int EntityObtainingFailure = 103;
+        
+        public const int EntitiesObtainingSuccess = 104;
+        public const int EntitiesObtainingFailure = 105;
+
+
+    }
+}

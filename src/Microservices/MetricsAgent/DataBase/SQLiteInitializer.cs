817bfd06059e21124cb0ab099808c0e6c1494bbc
Tarxos <ftarxos@gmail.com>
2021-05-03 01:39:34 +0300

    rework metric agent database initialization to use dapper

--- src/Microservices/MetricsAgent/DataBase/SQLiteInitializer.cs
+++ src/Microservices/MetricsAgent/DataBase/SQLiteInitializer.cs
@@ -1,5 +1,7 @@
+using System.Data;
 using System.Data.SQLite;
 using Bogus;
+using Dapper;
 using MetricsAgent.DataBase.Models;
 
 namespace MetricsAgent.DataBase
@@ -7,129 +9,135 @@ namespace MetricsAgent.DataBase
     public class SQLiteInitializer
     {
         private readonly SQLiteContainer _container;
-        
-        
+
+
         private Faker<CpuMetric> cpuMetricsGenerator = new Faker<CpuMetric>().Rules((f, m) =>
         {
             m.Value = f.Random.Int();
             m.Time = f.Date.RecentOffset(32).ToUnixTimeSeconds();
         });
+
         private Faker<DotnetMetric> dotnetMetricsGenerator = new Faker<DotnetMetric>().Rules((f, m) =>
         {
             m.Value = f.Random.Int();
             m.Time = f.Date.RecentOffset(32).ToUnixTimeSeconds();
         });
+
         private Faker<HddMetric> hddMetricsGenerator = new Faker<HddMetric>().Rules((f, m) =>
         {
             m.Value = f.Random.Int();
             m.Time = f.Date.RecentOffset(32).ToUnixTimeSeconds();
         });
+
         private Faker<NetworkMetric> networkMetricsGenerator = new Faker<NetworkMetric>().Rules((f, m) =>
         {
             m.Value = f.Random.Int();
             m.Time = f.Date.RecentOffset(32).ToUnixTimeSeconds();
         });
+
         private Faker<RamMetric> ramMetricsGenerator = new Faker<RamMetric>().Rules((f, m) =>
         {
             m.Value = f.Random.Int();
             m.Time = f.Date.RecentOffset(32).ToUnixTimeSeconds();
         });
 
-        
+
         public SQLiteInitializer(SQLiteContainer container)
         {
             _container = container;
         }
 
-        
+
         public void Init()
         {
             using var connection = _container.CreateConnection();
             connection.Open();
-            
+
             // CPU
-            RecreateTable(connection,"cpumetrics","id INTEGER PRIMARY KEY, value INT, time INT");
+            RecreateTable(connection, "cpumetrics", "id INTEGER PRIMARY KEY, value INT, time INT");
             foreach (var cpu in cpuMetricsGenerator.Generate(10))
-                AddCpuEntry(connection,cpu);
+                AddCpuEntry(connection, cpu);
             // DOTNET
-            RecreateTable(connection,"dotnetmetrics","id INTEGER PRIMARY KEY, value INT, time INT");
+            RecreateTable(connection, "dotnetmetrics", "id INTEGER PRIMARY KEY, value INT, time INT");
             foreach (var dotnet in dotnetMetricsGenerator.Generate(10))
-                AddDotnetEntry(connection,dotnet);
+                AddDotnetEntry(connection, dotnet);
             // HDD
-            RecreateTable(connection,"hddmetrics","id INTEGER PRIMARY KEY, value INT, time INT");
+            RecreateTable(connection, "hddmetrics", "id INTEGER PRIMARY KEY, value INT, time INT");
             foreach (var hdd in hddMetricsGenerator.Generate(10))
-                AddHddEntry(connection,hdd);
+                AddHddEntry(connection, hdd);
             // NETWORK
-            RecreateTable(connection,"networkmetrics","id INTEGER PRIMARY KEY, value INT, time INT");
+            RecreateTable(connection, "networkmetrics", "id INTEGER PRIMARY KEY, value INT, time INT");
             foreach (var network in networkMetricsGenerator.Generate(10))
-                AddNetworkEntry(connection,network);
+                AddNetworkEntry(connection, network);
             //RAM
-            RecreateTable(connection,"rammetrics","id INTEGER PRIMARY KEY, value INT, time INT");
+            RecreateTable(connection, "rammetrics", "id INTEGER PRIMARY KEY, value INT, time INT");
             foreach (var ram in ramMetricsGenerator.Generate(10))
-                AddRamEntry(connection,ram);
-            
+                AddRamEntry(connection, ram);
+
             connection.Close();
         }
 
-        
-        private void RecreateTable(SQLiteConnection connection, string tableName, string tableEntrySchema)
+
+        private void RecreateTable(IDbConnection connection, string tableName, string tableEntrySchema)
         {
-            using var command = new SQLiteCommand(connection);
-            command.CommandText = $"DROP TABLE IF EXISTS {tableName}";
-            command.ExecuteNonQuery();
-            command.CommandText = $"CREATE TABLE {tableName}({tableEntrySchema})";
-            command.ExecuteNonQuery();
+            connection.Execute($"DROP TABLE IF EXISTS {tableName}");
+            connection.Execute($"CREATE TABLE {tableName}({tableEntrySchema})");
         }
-        
-        
+
+
         private void AddCpuEntry(SQLiteConnection connection, CpuMetric metric)
         {
-            using var cmd = new SQLiteCommand(connection);
-            cmd.CommandText = "INSERT INTO cpumetrics(value,time) VALUES (@value,@time);";
-            cmd.Parameters.AddWithValue("@value", metric.Value);
-            cmd.Parameters.AddWithValue("@time", metric.Time);
-            cmd.Prepare();
-            cmd.ExecuteNonQuery();
+            connection.Execute(
+                "INSERT INTO cpumetrics(value,time) VALUES (@value,@time);",
+                new
+                {
+                    value = metric.Value,
+                    time = metric.Time
+                });
         }
-        
-        private void AddDotnetEntry(SQLiteConnection connection, DotnetMetric metric)
+
+        private void AddDotnetEntry(IDbConnection connection, DotnetMetric metric)
         {
-            using var cmd = new SQLiteCommand(connection);
-            cmd.CommandText = "INSERT INTO dotnetmetrics(value,time) VALUES (@value,@time);";
-            cmd.Parameters.AddWithValue("@value", metric.Value);
-            cmd.Parameters.AddWithValue("@time", metric.Time);
-            cmd.Prepare();
-            cmd.ExecuteNonQuery();
+            connection.Execute(
+                "INSERT INTO dotnetmetrics(value,time) VALUES (@value,@time);",
+                new
+                {
+                    value = metric.Value,
+                    time = metric.Time
+                });
         }
-        
-        private void AddHddEntry(SQLiteConnection connection, HddMetric metric)
+
+        private void AddHddEntry(IDbConnection connection, HddMetric metric)
         {
-            using var cmd = new SQLiteCommand(connection);
-            cmd.CommandText = "INSERT INTO hddmetrics(value,time) VALUES (@value,@time);";
-            cmd.Parameters.AddWithValue("@value", metric.Value);
-            cmd.Parameters.AddWithValue("@time", metric.Time);
-            cmd.Prepare();
-            cmd.ExecuteNonQuery();
+            connection.Execute(
+                "INSERT INTO hddmetrics(value,time) VALUES (@value,@time);",
+                new
+                {
+                    value = metric.Value,
+                    time = metric.Time
+                });
         }
-        
-        private void AddNetworkEntry(SQLiteConnection connection, NetworkMetric metric)
+
+        private void AddNetworkEntry(IDbConnection connection, NetworkMetric metric)
         {
-            using var cmd = new SQLiteCommand(connection);
-            cmd.CommandText = "INSERT INTO networkmetrics(value,time) VALUES (@value,@time);";
-            cmd.Parameters.AddWithValue("@value", metric.Value);
-            cmd.Parameters.AddWithValue("@time", metric.Time);
-            cmd.Prepare();
-            cmd.ExecuteNonQuery();
+            connection.Execute(
+                "INSERT INTO networkmetrics(value,time) VALUES (@value,@time);",
+                new
+                {
+                    value = metric.Value,
+                    time = metric.Time
+                });
         }
-        
-        private void AddRamEntry(SQLiteConnection connection, RamMetric metric)
+
+        private void AddRamEntry(IDbConnection connection, RamMetric metric)
         {
-            using var cmd = new SQLiteCommand(connection);
-            cmd.CommandText = "INSERT INTO rammetrics(value,time) VALUES (@value,@time);";
-            cmd.Parameters.AddWithValue("@value", metric.Value);
-            cmd.Parameters.AddWithValue("@time", metric.Time);
-            cmd.Prepare();
-            cmd.ExecuteNonQuery();
+            connection.Execute(
+                "INSERT INTO rammetrics(value,time) VALUES (@value,@time);",
+                new
+                {
+                    value = metric.Value,
+                    time = metric.Time
+                });
         }
     }
 }

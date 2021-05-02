2db38a406d7b3fa45ecc3303f525b5b90343cca9
Tarxos <ftarxos@gmail.com>
2021-05-02 22:11:52 +0300

    Metrics responses and updates for controllers to use repositories

--- src/Microservices/MetricsManager/Controllers/CpuMetricsController.cs
+++ src/Microservices/MetricsManager/Controllers/CpuMetricsController.cs
@@ -1,22 +1,72 @@
+using System;
+using System.Linq;
+using Common;
 using MetricsManager.Controllers.Requests;
+using MetricsManager.Controllers.Responses;
+using MetricsManager.DataBase.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace MetricsManager.Controllers
 {
     [Route("api/metrics/cpu")]
     public class CpuMetricsController : ApiController
     {
+        private readonly ICpuMetricsRepository _repository;
+        private readonly ILogger<CpuMetricsController> _logger;
+
+        public CpuMetricsController(ICpuMetricsRepository repository, ILogger<CpuMetricsController> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+        
+        
         [HttpGet("agent/{agentId}/from/{fromTime}/to/{toTime}")]
         public IActionResult GetMetricsFromAgent([FromRoute] CpuMetricsFromAgentRequest request)
         {
-            return Ok();
+            _logger.LogInformation(
+                LogEvents.RequestReceived,
+                "Get cpu metrics by time period request received: {From}, {To}, for agent: {AgentId}"
+                ,request.FromTime.ToString("yyyy-M-d dddd"),
+                request.ToTime.ToString("yyyy-M-d dddd"),
+                request.AgentId);
+
+            var result = _repository.GetByTimePeriod(request.FromTime, request.ToTime, request.AgentId);
+            if (result is null)
+            {
+                return NotFound();
+            }
+            return Ok(new CpuGetMetricsFromAgentResponse(){Metrics = result.Select(m =>new CpuMetricResponse()
+            {
+                Id = m.Id,
+                Time = DateTimeOffset.FromUnixTimeSeconds(m.Time),
+                Value = m.Value,
+                AgentId = m.AgentId
+            }).ToList()});
         }
 
         [HttpGet("cluster/from/{fromTime}/to/{toTime}")]
         public IActionResult GetMetricsFromAllCluster([FromRoute] CpuMetricsFromAllClusterRequest request)
         {
-            return Ok();
-        }
+            _logger.LogInformation(
+                LogEvents.RequestReceived,
+                "Get cpu metrics by time period request received: {From}, {To}"
+                ,request.FromTime.ToString("yyyy-M-d dddd"),
+                request.ToTime.ToString("yyyy-M-d dddd"));
 
+            var result = _repository.GetByTimePeriod(request.FromTime, request.ToTime);
+            if (result is null)
+            {
+                return NotFound();
+            }
+            return Ok(new CpuGetMetricsFromAllClusterResponse(){Metrics = result.Select(m =>new CpuMetricResponse()
+            {
+                Id = m.Id,
+                Time = DateTimeOffset.FromUnixTimeSeconds(m.Time),
+                Value = m.Value,
+                AgentId = m.AgentId
+            }).ToList()});
+        }
     }
 }
--- src/Microservices/MetricsManager/Controllers/RamMetricsController.cs
+++ src/Microservices/MetricsManager/Controllers/RamMetricsController.cs
@@ -1,21 +1,72 @@
+using System;
+using System.Linq;
+using Common;
 using MetricsManager.Controllers.Requests;
+using MetricsManager.Controllers.Responses;
+using MetricsManager.DataBase.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace MetricsManager.Controllers
 {
     [Route("api/metrics/ram")]
     public class RamMetricsController : ApiController
     {
+        private readonly IRamMetricsRepository _repository;
+        private readonly ILogger<RamMetricsController> _logger;
+
+        public RamMetricsController(IRamMetricsRepository repository, ILogger<RamMetricsController> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+        
+        
         [HttpGet("available/agent/{agentId}/from/{fromTime}/to/{toTime}")]
         public IActionResult GetAvailableSpaceInfoFromAgent([FromRoute] RamMetricsFromAgentRequest request)
         {
-            return Ok();
+            _logger.LogInformation(
+                LogEvents.RequestReceived,
+                "Get ram metrics by time period request received: {From}, {To}, for agent: {AgentId}"
+                ,request.FromTime.ToString("yyyy-M-d dddd"),
+                request.ToTime.ToString("yyyy-M-d dddd"),
+                request.AgentId);
+
+            var result = _repository.GetByTimePeriod(request.FromTime, request.ToTime, request.AgentId);
+            if (result is null)
+            {
+                return NotFound();
+            }
+            return Ok(new RamGetMetricsFromAgentResponse(){Metrics = result.Select(m =>new RamMetricResponse()
+            {
+                Id = m.Id,
+                Time = DateTimeOffset.FromUnixTimeSeconds(m.Time),
+                Value = m.Value,
+                AgentId = m.AgentId
+            }).ToList()});
         }
 
         [HttpGet("available/cluster/from/{fromTime}/to/{toTime}")]
         public IActionResult GetAvailableSpaceInfoFromAllCluster([FromRoute] RamMetricsFromAllClusterRequest request)
         {
-            return Ok();
+            _logger.LogInformation(
+                LogEvents.RequestReceived,
+                "Get ram metrics by time period request received: {From}, {To}"
+                ,request.FromTime.ToString("yyyy-M-d dddd"),
+                request.ToTime.ToString("yyyy-M-d dddd"));
+
+            var result = _repository.GetByTimePeriod(request.FromTime, request.ToTime);
+            if (result is null)
+            {
+                return NotFound();
+            }
+            return Ok(new RamGetMetricsFromAllClusterResponse(){Metrics = result.Select(m =>new RamMetricResponse()
+            {
+                Id = m.Id,
+                Time = DateTimeOffset.FromUnixTimeSeconds(m.Time),
+                Value = m.Value,
+                AgentId = m.AgentId
+            }).ToList()});
         }
     }
 }

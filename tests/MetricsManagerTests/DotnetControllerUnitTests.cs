ab6b2a797a9259fda48427db1971ded3afc38b4f
Tarxos <ftarxos@gmail.com>
2021-05-02 22:28:20 +0300

    update metrics manager tests

--- tests/MetricsManagerTests/DotnetControllerUnitTests.cs
+++ tests/MetricsManagerTests/DotnetControllerUnitTests.cs
@@ -1,7 +1,10 @@
 using System;
 using MetricsManager.Controllers;
 using MetricsManager.Controllers.Requests;
+using MetricsManager.DataBase.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
 using Xunit;
 
 namespace MetricsManagerTests
@@ -9,11 +12,13 @@ namespace MetricsManagerTests
     public class DotnetControllerUnitTests
     {
         private readonly DotnetMetricsController _controller;
-        
+        private readonly Mock<IDotnetMetricsRepository> _repoMock;
         
         public DotnetControllerUnitTests()
         {
-            _controller = new();
+            _repoMock = new();
+            Mock<ILogger<DotnetMetricsController>> loggerMock = new();
+            _controller = new(_repoMock.Object,loggerMock.Object);
         }
         
         
@@ -46,6 +51,65 @@ namespace MetricsManagerTests
 
             // Assert
             _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+        
+        [Fact]
+        public void GetMetricsFromAgent_VerifyRequestToRepository()
+        {
+            // Mock setup
+            _repoMock.Setup(repo =>
+                    repo
+                        .GetByTimePeriod(
+                            It.IsAny<DateTimeOffset>(),
+                            It.IsAny<DateTimeOffset>(),
+                            It.IsAny<int>()))
+                .Verifiable();
+            
+            //Arrange
+            var agentId = 1;
+            var fromTime = DateTimeOffset.FromUnixTimeSeconds(0);
+            var toTime = DateTimeOffset.FromUnixTimeSeconds(100);
+            var request = new ErrorsCountFromAgentRequest(agentId, fromTime, toTime);
+            
+            //Act
+            _ = _controller.GetErrorsCountFromAgent(request);
+
+            // Assert
+            _repoMock.Verify(repo => 
+                repo
+                    .GetByTimePeriod(
+                        It.IsAny<DateTimeOffset>(),
+                        It.IsAny<DateTimeOffset>(),
+                        It.IsAny<int>()
+                    ), Times.AtMostOnce());
+        }
+        
+        [Fact]
+        public void GetMetricsFromAllCluster_VerifyRequestToRepository()
+        {
+            // Mock setup
+            _repoMock.Setup(repo =>
+                    repo
+                        .GetByTimePeriod(
+                            It.IsAny<DateTimeOffset>(),
+                            It.IsAny<DateTimeOffset>()))
+                .Verifiable();
+            
+            //Arrange
+            var fromTime = DateTimeOffset.FromUnixTimeSeconds(0);
+            var toTime = DateTimeOffset.FromUnixTimeSeconds(100);
+            var request = new ErrorsCountFromAllClusterRequest(fromTime, toTime);
+
+            //Act
+            _ = _controller.GetErrorsCountFromAllCluster(request);
+
+            // Assert
+            _repoMock.Verify(repo => 
+                repo
+                    .GetByTimePeriod(
+                        It.IsAny<DateTimeOffset>(),
+                        It.IsAny<DateTimeOffset>()
+                    ), Times.AtMostOnce());
         }
     }
 }
--- tests/MetricsManagerTests/HddControllerUnitTests.cs
+++ tests/MetricsManagerTests/HddControllerUnitTests.cs
@@ -1,7 +1,10 @@
 using System;
 using MetricsManager.Controllers;
 using MetricsManager.Controllers.Requests;
+using MetricsManager.DataBase.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
 using Xunit;
 
 namespace MetricsManagerTests
@@ -9,11 +12,13 @@ namespace MetricsManagerTests
     public class HddControllerUnitTests
     {
         private readonly HddMetricsController _controller;
-        
+        private readonly Mock<IHddMetricsRepository> _repoMock;
         
         public HddControllerUnitTests()
         {
-            _controller = new();
+            _repoMock = new();
+            Mock<ILogger<HddMetricsController>> loggerMock = new();
+            _controller = new(_repoMock.Object,loggerMock.Object);
         }
         
         
@@ -46,6 +51,65 @@ namespace MetricsManagerTests
 
             // Assert
             _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+        
+        [Fact]
+        public void GetMetricsFromAgent_VerifyRequestToRepository()
+        {
+            // Mock setup
+            _repoMock.Setup(repo =>
+                    repo
+                        .GetByTimePeriod(
+                            It.IsAny<DateTimeOffset>(),
+                            It.IsAny<DateTimeOffset>(),
+                            It.IsAny<int>()))
+                .Verifiable();
+            
+            //Arrange
+            var agentId = 1;
+            var fromTime = DateTimeOffset.FromUnixTimeSeconds(0);
+            var toTime = DateTimeOffset.FromUnixTimeSeconds(100);
+            var request = new HddMetricsFromAgentRequest(agentId, fromTime, toTime);
+            
+            //Act
+            _ = _controller.GetLeftSpaceOnHddFromAgent(request);
+
+            // Assert
+            _repoMock.Verify(repo => 
+                repo
+                    .GetByTimePeriod(
+                        It.IsAny<DateTimeOffset>(),
+                        It.IsAny<DateTimeOffset>(),
+                        It.IsAny<int>()
+                    ), Times.AtMostOnce());
+        }
+        
+        [Fact]
+        public void GetMetricsFromAllCluster_VerifyRequestToRepository()
+        {
+            // Mock setup
+            _repoMock.Setup(repo =>
+                    repo
+                        .GetByTimePeriod(
+                            It.IsAny<DateTimeOffset>(),
+                            It.IsAny<DateTimeOffset>()))
+                .Verifiable();
+            
+            //Arrange
+            var fromTime = DateTimeOffset.FromUnixTimeSeconds(0);
+            var toTime = DateTimeOffset.FromUnixTimeSeconds(100);
+            var request = new HddMetricsFromAllClusterRequest(fromTime, toTime);
+
+            //Act
+            _ = _controller.GetLeftSpaceOnHddFromAllCluster(request);
+
+            // Assert
+            _repoMock.Verify(repo => 
+                repo
+                    .GetByTimePeriod(
+                        It.IsAny<DateTimeOffset>(),
+                        It.IsAny<DateTimeOffset>()
+                    ), Times.AtMostOnce());
         }
     }
 }

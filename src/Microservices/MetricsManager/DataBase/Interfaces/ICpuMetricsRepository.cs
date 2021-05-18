a65e4e64c103a758e74a6d794aa8e855c17deeaa
Tarxos <ftarxos@gmail.com>
2021-05-18 22:03:11 +0300

    prototype Get last date from database

--- src/Microservices/MetricsManager/DataBase/Interfaces/ICpuMetricsRepository.cs
+++ src/Microservices/MetricsManager/DataBase/Interfaces/ICpuMetricsRepository.cs
@@ -1,9 +1,10 @@
+using System;
 using MetricsManager.DataBase.Models;
 
 namespace MetricsManager.DataBase.Interfaces
 {
     public interface ICpuMetricsRepository : IRepository<CpuMetric,int,int>
     {
-        
+        DateTimeOffset GetAgentLastMetricDate(int agentId);
     }
 }

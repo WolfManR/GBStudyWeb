871262d2602c9fa1142b0f4d725f87634e4f65f5
Tarxos <ftarxos@gmail.com>
2021-05-25 22:41:39 +0300

    update agent controllers tests

--- tests/MetricsAgentTests/NetworkControllerUnitTests.cs
+++ tests/MetricsAgentTests/NetworkControllerUnitTests.cs
@@ -1,11 +1,15 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+
 using AutoMapper;
 
 using MetricsAgent;
 using MetricsAgent.Controllers;
 using MetricsAgent.Controllers.Requests;
+using MetricsAgent.Controllers.Responses;
 using MetricsAgent.DataBase.Interfaces;
-using Microsoft.AspNetCore.Mvc;
+using MetricsAgent.DataBase.Models;
 using Microsoft.Extensions.Logging;
 using Moq;
 using Xunit;
@@ -26,33 +30,90 @@ namespace MetricsAgentTests
                     expression.AddProfile(typeof(MapperProfile))));
             _controller = new(_repoMock.Object, loggerMock.Object, mapper);
         }
-        
+
         [Fact]
-        public void GetErrorsCount_ReturnsOk()
+        public void GetMetrics_WithEmptyRepository_ReturnsEmptyResponse()
         {
+            _repoMock.Setup(repo => repo
+                .GetByTimePeriod(
+                    It.IsAny<DateTimeOffset>(),
+                    It.IsAny<DateTimeOffset>()))
+                .Returns(() => Enumerable.Empty<NetworkMetric>().ToList());
             //Arrange
             var fromTime = DateTimeOffset.FromUnixTimeMilliseconds(0);
             var toTime = DateTimeOffset.FromUnixTimeMilliseconds(100);
-            NetworkMetricsRequest request = new(fromTime,toTime);
-            
+            NetworkMetricsRequest request = new(fromTime, toTime);
+
             //Act
             var result = _controller.GetByTimePeriod(request);
 
             // Assert
-            _ = Assert.IsAssignableFrom<IActionResult>(result);
+            _ = Assert.IsType<NetworkMetricsByTimePeriodResponse>(result);
+            Assert.Empty(result.Metrics);
         }
-        
+
+        [Fact]
+        public void GetMetrics_WithOneEntryInRepository_ReturnsResponseWithOneEntry()
+        {
+            _repoMock.Setup(repo => repo
+                    .GetByTimePeriod(
+                        It.IsAny<DateTimeOffset>(),
+                        It.IsAny<DateTimeOffset>()))
+                .Returns(() => new List<NetworkMetric>(){new()
+                {
+                    Time = DateTimeOffset.FromUnixTimeSeconds(10).ToUnixTimeSeconds(),
+                    Value = 1
+                }});
+            //Arrange
+            var fromTime = DateTimeOffset.FromUnixTimeMilliseconds(0);
+            var toTime = DateTimeOffset.FromUnixTimeMilliseconds(100);
+            NetworkMetricsRequest request = new(fromTime, toTime);
+
+            //Act
+            var result = _controller.GetByTimePeriod(request);
+
+            // Assert
+            _ = Assert.IsType<NetworkMetricsByTimePeriodResponse>(result);
+            Assert.Equal(1, result.Metrics.ToArray()[0].Value);
+        }
+
+        [Fact]
+        public void GetMetrics_WithOneEntryInRepository_ReturnsEmptyResponse_WhenEntryIsNotInTimeRange()
+        {
+            _repoMock.Setup(repo => repo
+                .GetByTimePeriod(
+                    It.Is<DateTimeOffset>(offset => offset.Millisecond > 10),
+                    It.Is<DateTimeOffset>(offset => offset.Millisecond > 10)))
+                .Returns(() => Enumerable.Empty<NetworkMetric>().ToList());
+
+            //Arrange
+            var fromTime = DateTimeOffset.FromUnixTimeMilliseconds(11);
+            var toTime = DateTimeOffset.FromUnixTimeMilliseconds(100);
+            NetworkMetricsRequest request = new(fromTime, toTime);
+
+            //Act
+            var result = _controller.GetByTimePeriod(request);
+
+            // Assert
+            _ = Assert.IsType<NetworkMetricsByTimePeriodResponse>(result);
+            Assert.Empty(result.Metrics);
+        }
+
         [Fact]
         public void GetFreeHardDriveSpace_VerifyRequestToRepository()
         {
             // Mock setup
-            _repoMock.Setup(repo =>
-                    repo
+            _repoMock.Setup(repo => repo
                         .GetByTimePeriod(
                             It.IsAny<DateTimeOffset>(),
                             It.IsAny<DateTimeOffset>()))
                 .Verifiable();
-            
+            _repoMock.Setup(repo => repo
+                    .GetByTimePeriod(
+                        It.IsAny<DateTimeOffset>(),
+                        It.IsAny<DateTimeOffset>()))
+                .Returns(() => Enumerable.Empty<NetworkMetric>().ToList());
+
             //Arrange
             var fromTime = DateTimeOffset.FromUnixTimeMilliseconds(0);
             var toTime = DateTimeOffset.FromUnixTimeMilliseconds(100);
